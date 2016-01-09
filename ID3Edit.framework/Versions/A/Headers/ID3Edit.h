aa196e324d23e973d43c9a353fd7af64c30c2c32
Philip Hardy <philiphardy4444@gmail.com>
2016-01-09 12:18:48 -0800

    Framework added

---
+++ ID3Edit.framework/Versions/A/Headers/ID3Edit.h
@@ -0,0 +1,19 @@
+//
+//  ID3Edit.h
+//  ID3Edit
+//
+//  Created by Philip Hardy on 1/6/16.
+//  Copyright © 2016 Hardy Creations. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ID3Edit.
+FOUNDATION_EXPORT double ID3EditVersionNumber;
+
+//! Project version string for ID3Edit.
+FOUNDATION_EXPORT const unsigned char ID3EditVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ID3Edit/PublicHeader.h>
+
+
